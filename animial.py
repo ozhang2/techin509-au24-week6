2475c70ecd942ac501b0511d7dca4a3657ec0f97
Oulu <oulu@OuludeMacBook-Pro.local>
2024-11-18 20:25:53 -0800

    add week 8

---
+++ animial.py
@@ -0,0 +1,42 @@
+#Object
+# -> Specific intance of a class
+
+# Class
+# -> General definition
+
+class iPhone:
+    def __init__(self, name, version, phone_number, color, model): # constructor
+        self.name = name
+        self.ios_version = version
+        self.phone_number = phone_number
+        self.color = color
+        self.model = model 
+        self.files = []
+
+    def call(self, number):
+        pass
+    
+    def airdrop(self, filename, recipient):
+        print("Dropping %s" % filename)
+        pass
+
+    def airreceive(self):
+        pass
+
+    def drop(self):
+        pass
+    def receive(self):
+        pass 
+
+# Create an instance of iPhone class
+ians_iphone = iPhone(
+    name="Ian's iPhone",
+    version="18",
+    phone_number = "123-123-1232",
+    color="qrey",
+    model="hi"
+)
+print(ians_iphone.name)
+print(ians_iphone.model)
+
+# rishis_iphone = iPhone(name="Rishi's iPhone")
---
+++ phone.py
@@ -0,0 +1,23 @@
+class Phone:
+    def __init__(self,number):
+        self.number = number
+
+    def call(self, to):
+        print('Dialing', to, "...")
+
+
+class iPhone(Phone):
+     def airdrop(self, to, file):
+          print('Sending', file, 'to', to, '...')
+
+class Android(Phone):
+     def ok_google(self):
+          print('Ok Google') 
+
+phone = iPhone()
+phone.call('123-123-5858')
+phone.airdrop()
+
+phone2 = Android()
+phone.call('123-123-5858')
+phone.okay_google()
